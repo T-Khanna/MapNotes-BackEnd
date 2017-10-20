0bdd689f0b2ea0c8e3ac9fbd08a0b1c4b9af5696
Harry Moore <harryonline3@hotmail.co.uk>
2017-10-20 21:36:02 +0100

    [HM] Modified delete query so it now expects an ID instead of a Title.

--- models/db.go
+++ models/db.go
@@ -60,8 +60,8 @@ func InsertNote(note Note) (id int64) {
 	return
 }
 
-func DeleteNote(title string) {
-	stmt, err := db.Prepare("DELETE FROM Notes WHERE title = $1")
+func DeleteNote(id int64) {
+	stmt, err := db.Prepare("DELETE FROM Notes WHERE id = $1")
 
 	if err != nil {
 		log.Fatal(err)
