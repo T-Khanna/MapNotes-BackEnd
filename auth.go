7c131ab9887cd58d5c98927b8c4a74df73786603
Harry7478 <harryegg7478@gmail.com>
2017-10-26 14:04:32 +0100

    [AA][HM] Changed auth function to also return a bool.

--- auth.go
+++ auth.go
@@ -12,30 +12,37 @@ Function takes a token and verifies the integrity of the token given
 After verification, it returns a user struct with the relevant information
 from the token
 */
-func AuthToken(token string) (models.User) {
+func AuthToken(token string) (isAuthenticated bool, user models.User) {
 
-  verify := googleAuthIDTokenVerifier.Verifier{}
-  aud := "xxxxxx-yyyyyyy.apps.googleusercontent.com"
+ verify := googleAuthIDTokenVerifier.Verifier{}
+ aud := "xxxxxx-yyyyyyy.apps.googleusercontent.com"
 
-  err := verify.VerifyIDToken(token, []string{aud,})
+ isAuthenticated = false
 
-  if err != nil {
-    log.Println(err.Error())
-    return models.User{Email: ""}
-  }
 
-  claimSet, err := googleAuthIDTokenVerifier.Decode(token)
+ err := verify.VerifyIDToken(token, []string{aud,})
 
-  if err != nil {
-    log.Println(err.Error())
-    return models.User{Email: ""}
-  }
 
-  // Get User email from claimSet
-  email := claimSet.Email
+ if err != nil {
+   log.Println(err.Error())
+   return false, models.User{Email: ""}
+ }
 
-  //Fill User struct
-  user := models.User{Email: email}
+ claimSet, err := googleAuthIDTokenVerifier.Decode(token)
 
-  return user
+ if err != nil {
+   log.Println(err.Error())
+   return false, models.User{Email: ""}
+ }
+
+ // Get User email from claimSet
+ email := claimSet.Email
+
+ //Fill User struct
+ user = models.User{Email: email}
+
+ //Token has been authenticated
+ isAuthenticated = true
+
+ return isAuthenticated, user
 }
