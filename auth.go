5a52492125a0909e8650ec5f304e7c12204e0ae1
Adanna <aa14415@ic.ac.uk>
2017-10-30 13:02:16 +0100

    [AA]: Added function AuthToken_test in auth.go This function checks that the result in AuthToken matches the result in the Google tokeninfo endpoint

--- auth.go
+++ auth.go
@@ -2,10 +2,30 @@ package main
 
 import (
   "log"
+  "net/http"
+  "encoding/json"
+  "io/ioutil"
 
   "github.com/futurenda/google-auth-id-token-verifier"
   "gitlab.doc.ic.ac.uk/g1736215/MapNotes/models"
 )
+
+type AuthUser struct {
+  Iss string `json:"iss"`
+  Sub string `json:"sub"`
+  Azp string `json:"azp"`
+  Aud string `json:"aud"`
+  Iat string `json:"iat"`
+  Exp string `json:"exp"`
+  Email string `json:"email"`
+  Email_verified string `json:"email_verified"`
+  Name string `json:"name"`
+  Picture string `json:"picture"`
+  Given_name string `json:"given_name"`
+  Family_name string `json:"family_name"`
+  Locale string `json:"locale"`
+}
+
 
 /*
 Function takes a token and verifies the integrity of the token given
@@ -45,4 +65,61 @@ func AuthToken(token string) (isAuthenticated bool, user models.User) {
  isAuthenticated = true
 
  return isAuthenticated, user
+}
+
+
+/*
+  Checks that function AuthToken returns same value as Google's tokeninfo endpoint
+  Result of AuthToken_test:
+  1. true, nil : Both AuthToken and tokeninfo endpoint return invalid token
+  2. true, AuthUser : Both AuthToken and tokeninfo endpoint have vaildated the
+                      token and given the same email address
+  3. false, nil: a) tokeninfo endpoint gives an invalid token but AuthToken gives
+                    a valid token or
+                 b) Both give valid token but email address gives are different
+*/
+func AuthToken_test(token string) (bool, *AuthUser) {
+  // Test token
+  tokeninfo := "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token
+  res, err := http.Get(tokeninfo)
+
+  // Actual
+  isAuthenticated, user := AuthToken(token)
+
+  if(err != nil) {
+    log.Println(err.Error())
+  }
+
+  // Google tokeninfo says: Invalid token received
+  if(res.StatusCode != http.StatusOK) {
+    //check that AuthToken returns false
+    return isAuthenticated == false, nil
+  }
+
+  // Get AuthUser Struct from Google tokeninfo endpoint
+  body, err := ioutil.ReadAll(res.Body)
+  if(err != nil) {
+    log.Println(err.Error)
+  }
+  auth_user := getAuthUser([]byte(body))
+
+  // Check email is the same
+  email_check := auth_user.Email == user.Email
+
+  if(email_check == false) {
+    return email_check, nil
+  }
+  return email_check, &auth_user
+}
+
+/*
+  Get AuthUser from JSON repsonse
+*/
+func getAuthUser(body []byte) (AuthUser) {
+    var auth_user AuthUser
+    err := json.Unmarshal(body, &auth_user)
+    if(err != nil){
+       log.Println(err.Error())
+    }
+    return auth_user
 }
